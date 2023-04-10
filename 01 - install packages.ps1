aea294c6c11cfbc835136522804ab91d8edf8f4c
Martin Furholt Bach <martin@furholt-bach.dk>
2023-04-10 13:31:16 +0200

    Initial commit

---
+++ 01 - install packages.ps1
@@ -0,0 +1,17 @@
+winget install Microsoft.VisualStudioCode -e --silent
+winget install Git.Git -e --silent
+winget install JetBrains.ToolBox -e --silent
+winget install Microsoft.DotNet.SDK.6 -e --silent
+winget install Microsoft.DotNet.SDK.7 -e --silent
+winget install Microsoft.PowerShell -e --silent
+winget install Microsoft.WindowsTerminal -e --silent
+winget install Fork.Fork -e --silent
+winget install Hashicorp.Terraform -e --silent
+winget install Microsoft.AzureCLI -e --silent
+winget install JanDeDobbeleer.OhMyPosh -e --silent
+winget install Postman.Postman -e --silent
+winget install Ghisler.TotalCommander -e --silent
+
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
+Write-Output "Install fonts, setup font in Terminal/PowerShell, setup powershell profile and OhMyPoshTheme"
---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+oh-my-posh init pwsh --config "f:\custom-theme-oh-my-posh.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons

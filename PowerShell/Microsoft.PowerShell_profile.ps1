cdcd15fdf63d52f1246035741615213a5010caa6
Martin Bach <82507174+Martin-Bach@users.noreply.github.com>
2023-10-06 08:57:41 +0200

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,11 @@
-oh-my-posh init pwsh --config ($HOME + "\custom-theme-oh-my-posh.json") | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\e144259\OneDrive - Mastercard\custom-theme-oh-my-posh.rev3.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
+#Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}

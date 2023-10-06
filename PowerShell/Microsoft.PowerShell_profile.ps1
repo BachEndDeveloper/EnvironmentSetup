9ace23af9f1efb75d1dd7bdbfbad62f6bc0829f8
Martin Bach <82507174+Martin-Bach@users.noreply.github.com>
2023-10-06 09:11:11 +0200

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
-$env:POSH_GIT_ENABLED = $true
 oh-my-posh init pwsh --config "C:\Users\e144259\OneDrive - Mastercard\custom-theme-oh-my-posh.rev3.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
+
+$env:POSH_GIT_ENABLED = $true
 #Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 # PowerShell parameter completion shim for the dotnet CLI
@@ -8,5 +9,15 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
     param($commandName, $wordToComplete, $cursorPosition)
         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
         }
 }
